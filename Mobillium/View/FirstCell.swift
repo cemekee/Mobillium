9e0f98d9e51a7a89aeb1924ec4c244d8455751a1
Cem Eke <cem.eke@pazarama.com>
2022-04-03 19:22:42 +0300

    page control added.

--- Mobillium/View/FirstCell.swift
+++ Mobillium/View/FirstCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class FirstCell: UITableViewCell {
     @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var pageControl: UIPageControl!
+    
     var goToDetail : (Int)->() = {_ in }
     private var data: [Result]?
     
@@ -21,12 +23,27 @@ class FirstCell: UITableViewCell {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(UINib(nibName: "SliderCell", bundle: nil), forCellWithReuseIdentifier: "SliderCell")
+        configurePageControl()
     }
 }
 
 extension FirstCell{
     func configure(data:[Result]?) {
         self.data = data
+    }
+    
+    private func configurePageControl(){
+        self.pageControl.numberOfPages = 5
+        self.pageControl.currentPage = 0
+        
+        let leading = NSLayoutConstraint(item: pageControl, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 0)
+        let trailing = NSLayoutConstraint(item: pageControl, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0)
+        let bottom = NSLayoutConstraint(item: pageControl, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        self.insertSubview(pageControl, at: 0)
+        self.bringSubviewToFront(pageControl)
+        self.addConstraints([leading, trailing, bottom])
+        self.addSubview(pageControl)
     }
 }
 extension FirstCell: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -42,23 +59,27 @@ extension FirstCell: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.goToDetail(data?[indexPath.row].id ?? 0)
-        //self.goToDetail(696806)
-//        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController
-//        vc?.selectedMovieId = data?[indexPath.row].id
-//        self.navigationController?.pushViewController(vc!, animated: true)
-        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offSet = scrollView.contentOffset.x
+        let width = scrollView.frame.width
+        let horizontalCenter = width / 2
+        
+        pageControl.currentPage = Int(offSet + horizontalCenter) / Int(width)
+    }
 }
 
 extension FirstCell: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-            return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+        return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            let gridLayout = collectionViewLayout as! UICollectionViewFlowLayout
-            let widthPerItem = collectionView.frame.width / 1 - gridLayout.minimumInteritemSpacing
-            let heightPerItem = collectionView.frame.height / 1 - gridLayout.minimumInteritemSpacing
-            return CGSize(width:widthPerItem, height:heightPerItem)
+        let gridLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        let widthPerItem = collectionView.frame.width / 1 - gridLayout.minimumInteritemSpacing
+        let heightPerItem = collectionView.frame.height / 1 - gridLayout.minimumInteritemSpacing
+        return CGSize(width:widthPerItem, height:heightPerItem)
         
     }
 }
